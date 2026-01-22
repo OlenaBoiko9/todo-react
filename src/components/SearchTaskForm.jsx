912d77e52ede1b02665e2f8df6b4310806172270
Elena4550 <elenaboiko7580@gmail.com>
2026-01-22 20:27:05 +0200

    Context API

--- src/components/SearchTaskForm.jsx
+++ src/components/SearchTaskForm.jsx
@@ -1,24 +1,28 @@
-import Field from "./Field"
+import Field from './Field'
+import { useContext } from 'react'
+import { TasksContext } from '../context/TasksContext'
 
-const SearchTaskForm = (props) => {
-    const {
-        searchQuery,
-        setSearchQuery,
-    } = props
+const SearchTaskForm = () => {
+  const {
+    searchQuery,
+    setSearchQuery,
+  } = useContext(TasksContext)
 
-    return (
-        <form className="todo__form"
-        onSubmit={(event) => event.preventDefault()}
-        >
-          <Field
-                className="todo__field"
-                label="Search task"
-                id="search"
-                value={searchQuery}
-                onInput={(event) => setSearchQuery(event.target.value)}
-            />
-      </form>
-    )
+  return (
+    <form
+      className="todo__form"
+      onSubmit={(event) => event.preventDefault()}
+    >
+      <Field
+        className="todo__field"
+        label="Search task"
+        id="search-task"
+        type="search"
+        value={searchQuery}
+        onInput={(event) => setSearchQuery(event.target.value)}
+      />
+    </form>
+  )
 }
 
 export default SearchTaskForm
